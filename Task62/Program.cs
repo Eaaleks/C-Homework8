db15cb3d8b59633eb4984f4df6b11cfaea99d1d3
thenewlife <thenewlife@mail.ru>
2023-02-21 21:11:44 +0300

    added Homework 8

---
+++ Task62/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[3,4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArraySpiral(int[,] matrix, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { matrix[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) matrix[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+int length = 4;
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix); 
+int[,] sqmatrix = new int[length, length];
+FillArraySpiral(sqmatrix, length);
+PrintMatrix(sqmatrix);
+
+
